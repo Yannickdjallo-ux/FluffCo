ddcb3afadffda8769608ad47baee357112420878
Yannickdjallo-ux <Hellodjallo@gmail.com>
2026-07-10 01:27:07 +0200

    Added wellness and how-its-made section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { BuyingModule } from "@/components/buying-module/buying-module"
 import { HeroSection } from "@/components/hero-section"
+import { HowItsMadeSection } from "@/components/how-its-made-section"
 import { TestimonialBar } from "@/components/testimonial-bar"
+import { WellnessSection } from "@/components/wellness-section"
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <HeroSection />
       <TestimonialBar />
       <BuyingModule />
+      <WellnessSection />
+      <HowItsMadeSection />
     </>
   )
 }
